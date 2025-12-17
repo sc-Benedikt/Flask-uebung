d0bcd52fca4257fde7518bd4e41c88ed11e7d2f4
Benedikt Mair <benedikt.mair@scaltel.de>
2025-12-17 13:17:54 +0100

    weiterleitung zu den seiten

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -10,13 +10,13 @@ def test():
         if username == "bene" and password == "123":
             return render_template("/mainmenu.html")
         else: 
-            return render_template("/eingabefeld.html")
+            new_title = "bitte feld ausfüllen"
+            return render_template("/eingabefeld.html", new_title= grund )
     return render_template("/eingabefeld.html")
 
 
-
-@app.route("/meinmenu")
-def show_name():
-    return render_template("/angecklickt.html")
+@app.route("/keinacc")
+def show_anmeldung():
+    return render_template("/registrierung.html")
 
 
