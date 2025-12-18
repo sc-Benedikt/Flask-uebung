5a6b0a5a461a245db9587c2c82a6a5835b2fdf3c
Benedikt Mair <benedikt.mair@scaltel.de>
2025-12-18 13:40:09 +0100

    gitignore test

--- hello.py
+++ hello.py
@@ -1,33 +1,37 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, url_for
 import json
-
-
 
 USER_FILE = r"C:\Übungen Python\git\Flask-uebung\user.json"
 
 
 
 app = Flask(__name__)
+app.secret_key = "(R&b5r8(BR85rb/)TtnN&T)ntNT)&n7t&TN)T&9nt9n"
 
 
 @app.route("/", methods=["GET", "POST"])
 def test():
-    if request.method == "POST":
+    name = session.get("username")
+    if name == None:
+        if request.method == "POST":
 
-        username = request.form.get("username", "")
-        password = request.form.get("password", "")
+            username = request.form.get("username", "")
+            password = request.form.get("password", "")
 
-        username = username.replace(" ","")
-        password = password.replace(" ", "")
-    
-        with open(USER_FILE, "r", encoding="utf-8") as f:
-            users = json.load(f)
-        if username in users and users[username] == password:
-            return render_template("mainmenu.html", name = username)
+            username = username.replace(" ","")
+            password = password.replace(" ", "")
+        
+            with open(USER_FILE, "r", encoding="utf-8") as f:
+                users = json.load(f)
+            if username in users and users[username] == password:
+                session["username"] = username
+                return render_template("mainmenu.html")
+            else:
+                return redirect("/")
         else:
             return render_template("eingabefeld.html")
-
-    return render_template("eingabefeld.html")
+    else:
+        return render_template("mainmenu.html")
 
 
 @app.route("/keinacc")
@@ -58,6 +62,13 @@ def neuer_acc():
 
     with open(r"C:\Übungen Python\git\Flask-uebung\user.json", "w") as f:
         f.write(json.dumps(users, indent=4))
+    return redirect("/")
+
+@app.route("/logout")
+def logout():
+
+    session.pop('username', None)
+    return redirect(url_for("test"))
     return redirect("/")
 
 
