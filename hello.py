1d6a8a6c4ee2af0de040649be28d0ee936ef482e
Benedikt Mair <benedikt.mair@scaltel.de>
2026-01-14 16:26:19 +0100

    absätze eingefügt

--- hello.py
+++ hello.py
@@ -3,8 +3,6 @@ import json
 
 USER_FILE = r"C:\Übungen Python\git\Flask-uebung\user.json"
 
-
-
 app = Flask(__name__)
 app.secret_key = "(R&b5r8(BR85rb/)TtnN&T)ntNT)&n7t&TN)T&9nt9n"
 
@@ -12,7 +10,9 @@ app.secret_key = "(R&b5r8(BR85rb/)TtnN&T)ntNT)&n7t&TN)T&9nt9n"
 @app.route("/", methods=["GET", "POST"])
 def test():
     name = session.get("username")
+
     if name == None:
+
         if request.method == "POST":
 
             username = request.form.get("username", "")
@@ -23,6 +23,7 @@ def test():
         
             with open(USER_FILE, "r", encoding="utf-8") as f:
                 users = json.load(f)
+
             if username in users and users[username] == password:
                 session["username"] = username
                 session["password"] = password
@@ -50,6 +51,7 @@ def neuer_acc():
 
     with open(r"C:\Übungen Python\git\Flask-uebung\user.json", "r") as f:
             content = f.read()
+
             if content:
                 users = json.loads(content)
             else:
@@ -57,6 +59,7 @@ def neuer_acc():
 
     if " " in new_user_name or " " in new_user_pw or "" == new_user_pw:
          return redirect("/neueracc")
+    
     if new_user_name in users:
         return render_template("exis_pw.html")
     else:
@@ -75,11 +78,14 @@ def logout():
 @app.route("/löschen")
 def delete():
     name = session.get("username")
+
     with open(USER_FILE, "r", encoding="utf-8") as f:
         users = json.load(f)
         del users[name]
+
         with open(r"C:\Übungen Python\git\Flask-uebung\user.json", "w") as f:
             f.write(json.dumps(users, indent=4))
+
         session.clear()
         return render_template("/eingabefeld.html")
 
@@ -89,20 +95,26 @@ def ame_aendern():
 
 @app.route("/geaendert", methods = ["POST"])
 def name_abgeaendert():
+
     old_name = session.get("username")
     old_password = session.get("password")
     password = request.form.get("password")
+
     if old_password == password:
         new_name = request.form.get("username", " ")
         with open(r"C:\Übungen Python\git\Flask-uebung\user.json", "r", encoding="utf-8") as f:
             users = json.load(f)
+
             if new_name in users:
                 return render_template("exis_pw.html")
+            
             else:
                 users[new_name] = old_password
 
             del users[old_name]
+
             users[new_name] = old_password
+
         with open(USER_FILE, "w") as f:
             f.write(json.dumps(users, indent=4))
             session["username"] = new_name
